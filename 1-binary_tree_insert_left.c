f3c6b47ba7bd81af966dcece60110341f67eeac3
Temesgen dora <tdemeke36@gmail.com>
2024-01-30 23:10:59 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (parent->right != NULL)
+	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
 		parent->left->parent = newNode;

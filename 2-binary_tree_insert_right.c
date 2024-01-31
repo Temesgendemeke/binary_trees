41de57eafb1c8c8441d2ea78a782e16960b93790
Temesgen dora <tdemeke36@gmail.com>
2024-01-30 22:27:03 -0800

    done: task 3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,12 +9,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	if (newNode == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
 	newNode = binary_tree_node(parent, value);
-	if (parent == NULL)
+	if (newNode == NULL)
 	{
 		return (NULL);
 	}

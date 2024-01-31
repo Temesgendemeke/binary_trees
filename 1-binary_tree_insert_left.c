f6264c2455b11397f6b55c5412034b0a238cd27f
Temesgen dora <tdemeke36@gmail.com>
2024-01-30 20:57:41 -0800

    git

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-
+    newNode->left->left = parent->left;
     parent->left = newNode->left;
    
     
